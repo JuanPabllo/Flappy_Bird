9f2f787bd11123303e1faa14254f7a7495a280d7
Juan <juanpablo192.dreaw@gmail.com>
2020-06-17 16:06:46 -0300

    js

--- js/flappy.js
+++ js/flappy.js
@@ -4,7 +4,7 @@ function novoElemento(tagName, className) {
     return elem
 }
 
-function Barreira (reversa = false) {
+function Barreira(reversa = false) {
     this.elemento = novoElemento('div', 'barreira')
 
     const borda = novoElemento('div', 'borda')
@@ -19,12 +19,29 @@ function Barreira (reversa = false) {
 // b.setAltura(300)
 // document.querySelector('[wm-flappy]').appendChild(b.elemento)
 
-function PardeBarreiras(altura, abertura, x) {
-    this.elemento = novoElemento('div', 'par-de-barrerias')
+function ParDeBarreiras(altura, abertura, x) {
+    this.elemento = novoElemento('div', 'par-de-barreiras')
 
     this.superior = new Barreira(true)
     this.inferior = new Barreira(false)
 
     this.elemento.appendChild(this.superior.elemento)
     this.elemento.appendChild(this.inferior.elemento)
+
+    this.sortearAbertura = () => {
+        const alturaSuperior = Math.random() * (altura - abertura)
+        const alturaInferior = altura - abertura - alturaSuperior
+        this.superior.setAltura(alturaSuperior)
+        this.inferior.setAltura(alturaInferior)
+    }
+
+    this.getX = () => parseInt(this.elemento.style.left.split('px')[0])
+    this.setX = x => this.elemento.style.left = `${x}px`
+    this.getLargura = () => this.elemento.clientWidth
+
+    this.sortearAbertura()
+    this.setX(x)
 }
+
+const b = new ParDeBarreiras(700, 200, 800)
+document.querySelector('[wm-flappy]').appendChild(b.elemento)
