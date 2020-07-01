0c68d1873eb218348ad0f68518e0958dc2762780
Juan Pablo <juanpablo192.dreaw@gmail.com>
2020-07-01 18:59:03 -0300

    Game over

--- js/flappy.js
+++ js/flappy.js
@@ -101,16 +101,7 @@ function Passaro(alturaJogo) {
     this.setY(alturaJogo / 2)
 }
 
-// const barreiras = new Barreiras(700, 1200,350, 400)
-// const passaro = new Passaro(700)
-// const areaDoJogo = document.querySelector('[wm-flappy]')
 
-// areaDoJogo.appendChild(passaro.elemento)
-// barreiras.pares.forEach(par => areaDoJogo.appendChild(par.elemento))
-// setInterval(() => {
-//     barreiras.animar()
-//     passaro.animar()
-// }, 20)
 
 function Progresso() {
     this.elemento = novoElemento('span', 'progresso')
@@ -120,6 +111,40 @@ function Progresso() {
     this.atualizarPontos(0)
 }
 
+// const barreiras = new Barreiras(700, 1200, 200, 400)
+// const passaro = new Passaro(700)
+// const areaDoJogo = document.querySelector('[wm-flappy]')
+// areaDoJogo.appendChild(passaro.elemento)
+// areaDoJogo.appendChild(new Progresso().elemento)
+// barreiras.pares.forEach(par => areaDoJogo.appendChild(par.elemento))
+// setInterval(() => {
+//     barreiras.animar()
+//     passaro.animar()
+// }, 20)
+
+function estaoSobrepostos(elementoA, elementoB) {
+    const a = elementoA.getBoundingClientRect()
+    const b = elementoB.getBoundingClientRect()
+
+    const horizontal = a.left + a.width >= b.left &&
+        b.left + b.width >= a.left
+    const vertical = a.top + a.height >= b.top &&
+        b.top + b.height >= a.top
+    return horizontal && vertical
+}
+
+function colidiu(passaro, barreiras) {
+    let colidiu = false
+    barreiras.pares.forEach(parDeBarreiras => {
+        if (!colidiu) {
+            const superior = parDeBarreiras.superior.elemento
+            const inferior = parDeBarreiras.inferior.elemento
+            colidiu = estaoSobrepostos(passaro.elemento, superior) ||
+                estaoSobrepostos(passaro.elemento, inferior)
+        }
+    })
+    return colidiu
+}
 
 function FlappyBird() {
     let pontos = 0
@@ -143,6 +168,9 @@ function FlappyBird() {
             barreiras.animar()
             passaro.animar()
 
+            if (colidiu(passaro, barreiras)) {
+                clearInterval(temporizador)
+            }
         }, 20)
     }
 }
